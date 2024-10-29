8fdd705f8884e35e3ba55a98509bd2832f3923fb
Sturle Tolo Nordeide <110419122+stunor92@users.noreply.github.com>
2024-10-29 20:18:54 +0100

    Remove tests

--- src/main/kotlin/no/stunor/origo/eventorapi/model/event/competitor/Result.kt
+++ src/main/kotlin/no/stunor/origo/eventorapi/model/event/competitor/Result.kt
@@ -1,10 +1,13 @@
 package no.stunor.origo.eventorapi.model.event.competitor
 
+import com.google.firebase.database.Exclude
 import java.io.Serializable
 
 data class Result(
         var time: Int? = null,
+        @Exclude
         var timeBehind: Int? =  null,
+        @Exclude
         val position: Int? = null,
         val status: ResultStatus = ResultStatus.OK
 ) : Serializable
--- src/test/java/no/stunor/origo/eventorapi/ApplicationTests.kt
+++
@@ -1,8 +0,0 @@
-package no.stunor.origo.eventorapi
-
-//@SpringBootTest
-internal class ApplicationTests {
-  //  @Test
-  //  fun contextLoads() {
-  //  }
-}
